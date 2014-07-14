586283c372221bdc0fa5c9664ccabc24a71ffe04
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-14 11:01:16 +0700

    Init basic working plugin

---
+++ includes/bank-bca.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Bank Transfer Payment Gateway
+ *
+ * Provides a Bank Transfer Payment Gateway. Based on WC_Gateway_Bank BCA class included on WooCommerce.
+ *
+ * @class 		WC_Gateway_Bank_BCA
+ * @extends		WC_Payment_Gateway
+ * @version		2.1.0
+ * @package		WooCommerce/Classes/Payment
+ * @author 		Fikri Rasyid
+ */
+class WC_Gateway_Bank_BCA extends WC_Payment_Gateway {
+
+    /**
+     * Constructor for the gateway.
+     */
+    public function __construct() {
+		$this->id                 = 'bank_bca';
+		$this->icon               = apply_filters('woocommerce_bank_bca_icon', '');
+		$this->has_fields         = false;
+		$this->method_title       = __( 'Bank BCA', 'woocommerce-indonesian-bank-payment-gateways' );
+		$this->method_description = __( 'Allows payments using direct bank/wire transfer by Bank BCA.', 'woocommerce-indonesian-bank-payment-gateways' );
+
+		// Load the settings.
+		$this->init_form_fields();
+		$this->init_settings();
+
+        // Define user set variables
+		$this->title        = $this->get_option( 'title' );
+		$this->description  = $this->get_option( 'description' );
+		$this->instructions = $this->get_option( 'instructions', $this->description );
+
+		// Bank BCA account fields shown on the thanks page and in emails
+		$this->account_details = get_option( 'woocommerce_bank_bca_accounts',
+			array(
+				array(
+					'account_name'   => $this->get_option( 'account_name' ),
+					'account_number' => $this->get_option( 'account_number' ),
+					'sort_code'      => $this->get_option( 'sort_code' ),
+					'bank_name'      => $this->get_option( 'bank_name' ),
+					'iban'           => $this->get_option( 'iban' ),
+					'bic'            => $this->get_option( 'bic' )
+				)
+			)
+		);
+
+		// Actions
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'save_account_details' ) );
+    	add_action( 'woocommerce_thankyou_bank_bca', array( $this, 'thankyou_page' ) );
+
+    	// Customer Emails
+    	add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
+    }	
+
+    /**
+     * Initialise Gateway Settings Form Fields
+     */
+    public function init_form_fields() {
+    	$this->form_fields = array(
+			'enabled' => array(
+				'title'   => __( 'Enable/Disable', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Enable Bank BCA Transfer', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default' => 'yes'
+			),
+			'title' => array(
+				'title'       => __( 'Title', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'        => 'text',
+				'description' => __( 'This title will be seen by the customer upon checkout process', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default'     => __( 'Direct Bank BCA Transfer', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'desc_tip'    => true,
+			),
+			'description' => array(
+				'title'       => __( 'Description', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'        => 'textarea',
+				'description' => __( 'Payment method description that the customer will see on your checkout.', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default'     => __( 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won\'t be shipped until the funds have cleared in our account.', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'desc_tip'    => true,
+			),
+			'instructions' => array(
+				'title'       => __( 'Instructions', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'        => 'textarea',
+				'description' => __( 'Instructions that will be added to the thank you page and emails.', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default'     => '',
+				'desc_tip'    => true,
+			),
+			'account_name' => array(
+				'title'       => __( 'Account Name', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'        => 'text',
+				'description' => __( 'This account name is displayed during checkout process and related emails to the customer', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default'     => __( 'John Doe', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'desc_tip'    => true,
+			),
+			'account_number' => array(
+				'title'       => __( 'Account Number', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'type'        => 'text',
+				'description' => __( 'This account number is displayed during checkout process and related emails to the customer', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'default'     => __( '0381912123819', 'woocommerce-indonesian-bank-payment-gateways' ),
+				'desc_tip'    => true,
+			),
+		);
+    }
+
+    /**
+     * Output for the order received page.
+     */
+    public function thankyou_page( $order_id ) {
+		if ( $this->instructions ) {
+        	echo wpautop( wptexturize( wp_kses_post( $this->instructions ) ) );
+        }
+        $this->bank_details( $order_id );
+    }    
+
+    /**
+     * Add content to the WC emails.
+     *
+     * @access public
+     * @param WC_Order $order
+     * @param bool $sent_to_admin
+     * @param bool $plain_text
+     * @return void
+     */
+    public function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
+    	if ( ! $sent_to_admin && 'bacs' === $order->payment_method && 'on-hold' === $order->status ) {
+			if ( $this->instructions ) {
+				echo wpautop( wptexturize( $this->instructions ) ) . PHP_EOL;
+			}
+			$this->bank_details( $order->id );
+		}
+    }
+
+    /**
+     * Get bank details and place into a list format
+     */
+    private function bank_details( $order_id = '' ) {
+    	if ( empty( $this->account_details ) ) {
+    		return;
+    	}
+
+    	echo '<h2>' . __( 'Our Bank BCA Account', 'woocommerce-indonesian-bank-payment-gateways' ) . '</h2>' . PHP_EOL;
+
+    	$bank_bca_accounts = apply_filters( 'woocommerce_bank_bca_accounts', $this->account_details );
+
+    	if ( ! empty( $bank_bca_accounts ) ) {
+	    	foreach ( $bank_bca_accounts as $bank_bca_account ) {
+	    		echo '<ul class="order_details bank_bca_details">' . PHP_EOL;
+
+	    		$bank_bca_account = (object) $bank_bca_account;
+
+	    		// Bank BCA fields shown on the thanks page and in emails
+				$account_fields = apply_filters( 'woocommerce_bank_bca_account_fields', array(
+					'account_name'=> array(
+						'label' => __( 'Account Name', 'woocommerce-indonesian-bank-payment-gateways' ),
+						'value' => $bank_bca_account->account_name
+					),
+					'account_number'=> array(
+						'label' => __( 'Account Number', 'woocommerce-indonesian-bank-payment-gateways' ),
+						'value' => $bank_bca_account->account_number
+					),
+				), $order_id );
+
+				if ( $bank_bca_account->account_name || $bank_bca_account->bank_name ) {
+					echo '<h3>' . implode( ' - ', array_filter( array( $bank_bca_account->account_name, $bank_bca_account->bank_name ) ) ) . '</h3>' . PHP_EOL;
+				}
+
+	    		foreach ( $account_fields as $field_key => $field ) {
+				    if ( ! empty( $field['value'] ) ) {
+				    	echo '<li class="' . esc_attr( $field_key ) . '">' . esc_attr( $field['label'] ) . ': <strong>' . wptexturize( $field['value'] ) . '</strong></li>' . PHP_EOL;
+				    }
+				}
+
+	    		echo '</ul>';
+	    	}
+	    }
+    }
+
+    /**
+     * Process the payment and return the result
+     *
+     * @param int $order_id
+     * @return array
+     */
+    public function process_payment( $order_id ) {
+
+		$order = new WC_Order( $order_id );
+
+		// Mark as on-hold (we're awaiting the payment)
+		$order->update_status( 'on-hold', __( 'Awaiting payment through', 'woocommerce-indonesian-bank-payment-gateways' ) );
+
+		// Reduce stock levels
+		$order->reduce_order_stock();
+
+		// Remove cart
+		WC()->cart->empty_cart();
+
+		// Return thankyou redirect
+		return array(
+			'result' 	=> 'success',
+			'redirect'	=> $this->get_return_url( $order )
+		);
+    }
+}
---
+++ woocommerce-indonesian-bank-payment-gateways.php
@@ -0,0 +1,45 @@
+<?php
+/*
+    Plugin Name: WooCommerce Indonesian Bank Payment Gateways
+    Version: 0.1
+    Description: Adding Indonesian Banks to WooCommerce's payment gateway option
+    Author: Fikri Rasyid
+    Author URI: http://fikrirasyid.com
+*/
+/*
+    Copyright 2014 Fikri Rasyid
+    Developed by Fikri Rasyid (fikrirasyid@gmail.com)
+*/
+
+class WC_Gateway_Indonesian_Banks_Setup{
+
+	function __construct(){
+		add_action( 'plugins_loaded', 				array( $this, 'load' ) );
+		add_filter( 'woocommerce_payment_gateways', array( $this, 'register') );
+	}
+
+	/**
+	 * Requiring files
+	 * 
+	 * @return void
+	 */
+	function load(){
+		require_once( dirname( __FILE__ ) . '/includes/bank-bca.php' );
+		require_once( dirname( __FILE__ ) . '/includes/bank-mandiri.php' );
+	}
+
+	/**
+	 * Register payment gateways
+	 * 
+	 * @param array
+	 * 
+	 * @return array
+	 */
+	function register( $methods ){
+		$methods[] = 'WC_Gateway_Bank_BCA';
+		$methods[] = 'WC_Gateway_Bank_Mandiri';
+
+		return $methods;
+	}
+}
+new WC_Gateway_Indonesian_Banks_Setup;
